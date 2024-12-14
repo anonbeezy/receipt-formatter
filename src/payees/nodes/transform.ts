ad14c90118d052040e1edaf2d4d1b7b3330ea3cc
Beezy <anonbeezy@proton.me>
2024-12-14 21:00:26 +0800

    fix(payees): implement Runnable interface

--- src/payees/nodes/transform.ts
+++ src/payees/nodes/transform.ts
@@ -1,31 +1,34 @@
-import { ToolNode } from '@langchain/langgraph/prebuilt';
-
 import { ChatOpenAI } from '@langchain/openai';
 import { PayeesState } from '../state';
 import { template } from '../template';
 import { PayeesService } from '../payees.service';
 import { Injectable, Logger } from '@nestjs/common';
+import { Runnable } from '@langchain/core/runnables';
+
+type Input = typeof PayeesState.State;
+type Output = typeof PayeesState.Update;
 
 @Injectable()
-export class TransformNode extends ToolNode {
+export class TransformNode extends Runnable<Input, Output> {
+  lc_namespace = ['payees', 'nodes'];
   private readonly logger = new Logger(TransformNode.name);
   constructor(
     private readonly model: ChatOpenAI,
     private readonly payeesService: PayeesService,
   ) {
-    super([]);
+    super();
   }
 
-  async run(state: typeof PayeesState.State) {
+  async invoke(input: Input): Promise<Output> {
     this.logger.log('Executing transformNode');
 
     const result = await template.pipe(this.model).invoke({
       csv: await this.payeesService.findAllAsCsv(),
-      input: state.payeeName,
+      input: input.payeeName,
     });
 
     return {
-      rawPayee: result.content,
+      rawPayee: result.content as string,
     };
   }
 }
